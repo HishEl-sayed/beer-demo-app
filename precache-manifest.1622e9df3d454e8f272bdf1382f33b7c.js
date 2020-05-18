1800ec0731596d4dbdc12de1577f40b13ef31e51
Hisham El-sayed <hisham.el-sayed@macbook-pro.home>
2020-05-18 03:40:37 +0100

    Updates

---
+++ precache-manifest.1622e9df3d454e8f272bdf1382f33b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a011657b58ee2f23ea2f1439944ed5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad44763bcc66f7020e18",
+    "url": "/static/css/main.51db8fe8.chunk.css"
+  },
+  {
+    "revision": "2df485a09dbffd57d462",
+    "url": "/static/js/2.d8d862fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8d862fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad44763bcc66f7020e18",
+    "url": "/static/js/main.c53c6b96.chunk.js"
+  },
+  {
+    "revision": "4d7aa6d64191d2a72dac",
+    "url": "/static/js/runtime-main.3b0c8a21.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beer-demo-app/precache-manifest.e89a88015d10901d5d017c435a107ce5.js"
+  "/precache-manifest.1622e9df3d454e8f272bdf1382f33b7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beer-demo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
